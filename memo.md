c6518c2015fb7092ad2d9a4779c21eacca4c825c
Kanghee Park <kpark247@wisc.edu>
2022-11-21 21:30:44 -0600

    basic option parsing

---
+++ memo.md
@@ -0,0 +1,3 @@
+# Packages to use
+
+* rsmt2 for cvc4 and z3 [https://docs.rs/rsmt2/latest/rsmt2/index.html]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+use std::{fs, path::PathBuf};
+
+#[derive(Parser)]
+#[clap(version, author, about)]
+struct Opts {
+    // The input file. The default is stdin.
+    file: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+    let input: String = fs::read_to_string(opts.file)
+        .expect("Error reading input");
+
+    println!("{}", input);
 }
